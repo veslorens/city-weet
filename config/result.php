92a9c02f71bd68d013f55e7bf704a765c4ca7db2
Lorenzo Yee <vhezyrenz@yahoo.com>
2016-11-05 10:36:12 +0800

    Add configuration for search results

---
+++ config/result.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /**
+     * Radius of the search
+     */
+    'radius' => '50km',
+
+    /**
+     * Number of results to display
+     */
+    'size' => 20,
+
+    /**
+     * TTL (time to live) of the cache (in seconds)
+     */
+    'TTL' => 3600,
+];
