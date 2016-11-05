8c6c73c1b300cbb165f0f270c7ab859721febf54
Lorenzo Yee <vhezyrenz@yahoo.com>
2016-11-05 10:30:43 +0800

    Add routes for Tweet feature

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+Route::get('tweet/find', 'TweetController@find')->name('tweet.find');
